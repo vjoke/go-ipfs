3ee3d15dc2e4eb0fb1791f4a544fa5053fbbeac5
Steven Allen <steven@stebalien.com>
2018-03-09 13:37:53 -0800

    avoid reading no bytes with no errors in the channel marshaler
    This is strongly discouraged by the go documentation and can make bad things
    happen.
    
    License: MIT
    Signed-off-by: Steven Allen <steven@stebalien.com>

--- commands/channelmarshaler.go
+++ commands/channelmarshaler.go
@@ -13,30 +13,35 @@ type ChannelMarshaler struct {
 }
 
 func (cr *ChannelMarshaler) Read(p []byte) (int, error) {
-	if cr.reader == nil {
-		val, more := <-cr.Channel
-		if !more {
-			//check error in response
-			if cr.Res.Error() != nil {
-				return 0, cr.Res.Error()
+	for {
+		if cr.reader == nil {
+			val, more := <-cr.Channel
+			if !more {
+				//check error in response
+				if cr.Res.Error() != nil {
+					return 0, cr.Res.Error()
+				}
+				return 0, io.EOF
 			}
-			return 0, io.EOF
+
+			r, err := cr.Marshaler(val)
+			if err != nil {
+				return 0, err
+			}
+			if r == nil {
+				continue
+			}
+			cr.reader = r
 		}
 
-		r, err := cr.Marshaler(val)
-		if err != nil {
-			return 0, err
+		n, err := cr.reader.Read(p)
+		if err == io.EOF {
+			cr.reader = nil
+			err = nil
+			if n == 0 {
+				continue
+			}
 		}
-		if r == nil {
-			return 0, nil
-		}
-		cr.reader = r
+		return n, err
 	}
-
-	n, err := cr.reader.Read(p)
-	if err == io.EOF {
-		cr.reader = nil
-		err = nil
-	}
-	return n, err
 }
